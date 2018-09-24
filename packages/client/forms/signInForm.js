4faa15682233e42fc8bc8a039533158eff0703da
IsuruNana <ipnanayakkara@crimson.ua.edu>
2018-09-23 20:29:37 -0500

    Added Formik forms
    Need to test with front end after it is setup

---
+++ packages/client/forms/signInForm.js
@@ -0,0 +1,28 @@
+import React from "React"
+import { render } from "react-dom"
+import { withFormik, Form, Field } from "formik"
+import Yup from "yup"
+
+const SignInForm = ({ values, handleChange }) => {
+  return (
+    <Form>
+      <Field type="email" name="email" placeholder="Email" />
+      <Field type="password" name="password" placeholder="Password" />
+      <button>Submit</button>
+    </Form>
+  )
+}
+
+const FormikForm = withFormik({
+  mapPropsToValues() {
+    return {
+      email: "",
+      password: "",
+    }
+  },
+  handleSubmit(values) {
+    //Make GraphQL request
+  },
+})(SignInForm)
+
+export default FormikForm
